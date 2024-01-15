d233bcacccb26b590602f0e75a20f2e57e6dcba1
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2024-01-15 14:43:04 +0100

    more error handlers added

--- src/api/errorsHandler.js
+++ src/api/errorsHandler.js
@@ -9,3 +9,44 @@ export const badRequestHandler = (err, req, res, next) => {
     next(er);
   }
 };
+
+export const forbiddenHandler = (err, req, res, next) => {
+  if (err.status === 403) {
+    res.status(403).send({
+      success: false,
+      message: err.message,
+    });
+  } else {
+    next(err);
+  }
+};
+
+export const unauthorizedHandler = (err, req, res, next) => {
+  if (err.status === 401) {
+    res.status(401).send({
+      success: false,
+      message: err.message,
+    });
+  } else {
+    next(err);
+  }
+};
+
+export const notFoundHandler = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({
+      success: false,
+      message: err.message,
+    });
+  } else {
+    next(err);
+  }
+};
+
+export const genericErrorHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({
+    success: false,
+    message: "Generic Server Error",
+  });
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 import Express from "express";
 import listEndpoints from "express-list-endpoints";
 import booksRouter from "./api/books/index.js";
+import {
+  badRequestHandler,
+  forbiddenHandler,
+  genericErrorHandler,
+  notFoundHandler,
+  unauthorizedHandler,
+} from "./api/errorsHandler.js";
 import usersRouter from "./api/users/index.js";
 
 const server = Express();
@@ -31,6 +38,14 @@ server.use(Express.json());
 // ********************** ENDPOINTS **********************
 server.use("/users", usersRouter);
 server.use("/books", booksRouter);
+
+// ********************** ERROR HANDLERS **********************
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
